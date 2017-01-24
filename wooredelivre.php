34adcf1654dbf5ba2e7bbbacfc5be7e8ceeb5e2a
Maurilio Atila <cabelotaina@gmail.com>
2017-01-24 16:04:51 -0200

    added birthday field for checkout

--- wooredelivre.php
+++ wooredelivre.php
@@ -24,13 +24,13 @@
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
     // Put your plugin code here
 }
-if ( ! defined( 'ABSPATH' ) ) { 
+if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
 // add_filter('gettext', 'change_checkout_btn');
 // add_filter('ngettext', 'change_checkout_btn');
-// 
+//
 // //function
 // function change_checkout_btn($checkout_btn){
 //   $checkout_btn= str_ireplace('Company Name', 'CPF', $checkout_btn);
@@ -46,5 +46,52 @@ function custom_override_checkout_fields( $fields ) {
      return $fields;
 }
 
+// ADD Custom Fields to Checkout Page
+/**
+ * Add the field to the checkout
+ **/
 
+add_action('woocommerce_after_order_notes', 'my_custom_checkout_field');
 
+function my_custom_checkout_field( $checkout ) {
+
+    date_default_timezone_set('America/Sao_Paulo');
+    $mydateoptions = array('' => __('Select BirthDay', 'woocommerce' ));
+
+    echo '<div id="my_custom_checkout_field"><h3>'.__('Informacionais Adicionais').'</h3>';
+
+   woocommerce_form_field( 'birth_date', array(
+        'type'          => 'text',
+        'class'         => array('my-field-class form-row-wide'),
+        'id'            => 'datepicker',
+        'required'      => true,
+        'label'         => __('Data de Nascimento'),
+        'placeholder'       => __('Select Date'),
+        'options'     =>   $mydateoptions
+      ),$checkout->get_value( 'birth_date' ));
+
+    echo '</div>';
+}
+
+/**
+ * Process the checkout
+ **/
+add_action('woocommerce_checkout_process', 'my_custom_checkout_field_process');
+
+function my_custom_checkout_field_process() {
+    global $woocommerce;
+
+    // Check if set, if its not set add an error.
+    if (!$_POST['birth_date'])
+         wc_add_notice( '<strong>Data de Nascimento</strong> ' . __( 'is a required field.', 'woocommerce' ), 'error' );
+}
+
+/**
+ * Update the order meta with field value
+ **/
+add_action('woocommerce_checkout_update_order_meta', 'my_custom_checkout_field_update_order_meta');
+
+function my_custom_checkout_field_update_order_meta( $order_id ) {
+    if ($_POST['birth_date']) update_post_meta( $order_id, 'BirthDay', esc_attr($_POST['birth_date']));
+}
+?>
